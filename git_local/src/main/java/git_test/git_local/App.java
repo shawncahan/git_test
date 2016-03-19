0ed42adb000f962e7297820286c023df30c6e836
shawn <shawn.cahan@outlook.com>
2016-03-19 13:16:17 -0600

    First Commit

---
+++ git_local/src/main/java/git_test/git_local/App.java
@@ -0,0 +1,13 @@
+package git_test.git_local;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
