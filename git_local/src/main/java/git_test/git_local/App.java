01ea2f00bbe91d1eadc4b3a954b41bab0906a106
shawn <shawn.cahan@outlook.com>
2016-03-19 21:16:27 -0600

    Version 2 commit

--- git_local/src/main/java/git_test/git_local/App.java
+++ git_local/src/main/java/git_test/git_local/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldzz!" );
+    	//Version 2
+        System.out.println( "Hello Worldzz2!" );
     }
 }
